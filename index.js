4dc9fb498be9acbfed721644f314828b261b54f3
Farves-744 <farves@ahsansolutions.com>
2023-05-13 19:41:00 +0530

    changes

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 const navLinks = document.querySelector(".nav-links");
-function onToggleMenu() {
-  //   e.name = e.name === "menu" ? "close" : "menu";
-  // navLinks.classList.toggle("top-[100%]");
-  navLinks.classList.toggle("top-[30rem]");
-}
-
+const links = document.querySelectorAll(".nav-links a");
+const navBarLinks = document.querySelectorAll(".nav-links ul");
 const hamburger = document.getElementById("nav-toggle");
 
 hamburger.onclick = function () {
   navLinks.classList.toggle("active");
 };
 
-// nav.click(function () {
-//   this.getElementById("menu").classList.toggle("active");
-// });
-
-const links = document.querySelectorAll(".nav-links a");
-const navBarLinks = document.querySelectorAll(".nav-links ul");
-
 links.forEach((l) => {
   l.addEventListener("click", () => {
     navLinks.classList.remove("active");
   });
 });
+
+// Disable right-click
+// document.addEventListener("contextmenu", (e) => e.preventDefault());
+
+// function ctrlShiftKey(e, keyCode) {
+//   return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+// }
+
+// document.onkeydown = (e) => {
+//   // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+//   if (event.keyCode === 123 || ctrlShiftKey(e, "I") || ctrlShiftKey(e, "J") || ctrlShiftKey(e, "C") || (e.ctrlKey && e.keyCode === "U".charCodeAt(0))) return false;
+// };
