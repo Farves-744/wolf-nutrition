ce517fc9a8d1ff754531c40652ab5eba4c8f02e6
Farves-744 <128053117+Farves-744@users.noreply.github.com>
2023-05-13 08:51:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const navLinks = document.querySelector(".nav-links");
 function onToggleMenu() {
   //   e.name = e.name === "menu" ? "close" : "menu";
-  navLinks.classList.toggle("top-[3rem]");
+  // navLinks.classList.toggle("top-[100%]");
+  navLinks.classList.toggle("top-[30rem]");
 }
 
-const nav = document.getElementById("nav-toggle");
+const hamburger = document.getElementById("nav-toggle");
 
-nav.click(function () {
-  navLinks.classList.toggle("top-[100%]");
-});
+hamburger.onclick = function () {
+  navLinks.classList.toggle("active");
+};
 
-nav.click(function () {
-  this.getElementById("menu").classList.toggle("active");
+// nav.click(function () {
+//   this.getElementById("menu").classList.toggle("active");
+// });
+
+const links = document.querySelectorAll(".nav-links a");
+const navBarLinks = document.querySelectorAll(".nav-links ul");
+
+links.forEach((l) => {
+  l.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
 });
